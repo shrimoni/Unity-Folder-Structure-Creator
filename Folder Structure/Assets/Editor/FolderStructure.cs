ae7fc8d2007ff5f9d3ef231b44d6f0b8ad3eebc7
Shriram <shrirammoni@gmail.com>
2019-05-19 14:07:44 +0530

    Initial Commit

---
+++ Folder Structure/Assets/Editor/FolderStructure.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class FolderStructure : EditorWindow
+{
+    static List<string> folderNames = new List<string>()
+    {
+        "Animations",
+        "Editor",
+        "Materials",
+        "Models",
+        "Plugins",
+        "Prefabs",
+        "Resources",
+        "Scenes",
+        "Scripts",
+        "Shaders",
+        "Sounds",
+        "Sprites",
+        "Textures"
+    };
+
+    [MenuItem("Tools/CreateFolderStructure")]
+    private static void Init()
+    {
+        CreateFolderStructure();
+    }
+
+    // Creates the folder in the given name
+    static void CreateFolder(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Debug.Log("Folder doesn't exists, thus creating a new folder: " + Path.GetFileName(path));
+            Directory.CreateDirectory(path);
+        }
+    }
+
+    // Creates the folder structure for the items in the list - folderNamesList
+    static void CreateFolderStructure()
+    {
+        if (folderNames.Count > 0)
+        {
+            var assetPath = Application.dataPath;
+            foreach (var name in folderNames)
+            {
+                var folderPath = Path.Combine(assetPath, name);
+                CreateFolder(folderPath);
+            }
+
+            AssetDatabase.Refresh();
+        }
+    }
+
+}
